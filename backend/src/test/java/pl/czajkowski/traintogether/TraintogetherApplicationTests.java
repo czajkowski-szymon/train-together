ec380355ad1d1d32c16f083e9e6b9b6c12f9f7d1
Szymon Czajkowski <sczajkowski83@gmail.com>
2024-03-18 12:15:23 +0100

    frontend and backend projects created, postgres set up with docker

---
+++ backend/src/test/java/pl/czajkowski/traintogether/TraintogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.czajkowski.traintogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraintogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
