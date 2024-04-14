87dbb1804b8541cba42209fbf443e1a47272518a
Szymon Czajkowski <sczajkowski83@gmail.com>
2024-04-14 22:47:03 +0200

    login, registration, discover views created

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  fb: FormBuilder = inject(FormBuilder);
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  onSubmit(): void {
+    console.log('onSubmit method');
+  }
+}
