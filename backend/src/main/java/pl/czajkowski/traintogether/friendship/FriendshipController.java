5ec219d4571816bd057bea0159cfdcb8d427289e
Szymon Czajkowski <sczajkowski83@gmail.com>
2024-04-26 19:45:12 +0200

    friendship and frirendship invitation controller added

---
+++ backend/src/main/java/pl/czajkowski/traintogether/friendship/FriendshipController.java
@@ -0,0 +1,12 @@
+package pl.czajkowski.traintogether.friendship;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/friendships")
+public class FriendshipController {
+
+
+
+}
