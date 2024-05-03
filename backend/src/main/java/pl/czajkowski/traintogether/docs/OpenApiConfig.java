a3d5890f5069da17ffc3adb3ce6dfd49874d23a8
Szymon Czajkowski <sczajkowski83@gmail.com>
2024-05-03 11:05:46 +0200

    open api config added

---
+++ backend/src/main/java/pl/czajkowski/traintogether/docs/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package pl.czajkowski.traintogether.docs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Szymon Czajkowski",
+                        email = "sczajkowski83@gmail.com"
+                ),
+                description = "OpenApi documentation for Train Together API",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local Environment",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
