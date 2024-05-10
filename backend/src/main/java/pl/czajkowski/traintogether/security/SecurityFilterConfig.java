86c1adbc9b1c6910982e5fe820c0868257b8e8df
Szymon Czajkowski <sczajkowski83@gmail.com>
2024-05-10 18:27:17 +0200

    cors config added

--- backend/src/main/java/pl/czajkowski/traintogether/security/SecurityFilterConfig.java
+++ backend/src/main/java/pl/czajkowski/traintogether/security/SecurityFilterConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
@@ -17,40 +22,47 @@ import static pl.czajkowski.traintogether.user.models.Role.ADMIN;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityFilterConfig {
 
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers(
-                                        "/api/v1/users/register",
-                                        "/api/v1/auth/login",
-                                        "/v3/api-docs/**",
-                                        "/swagger-ui/**"
-                                )
-                                    .permitAll()
-
-                                .requestMatchers( "/api/v1/admin/**")
-                                    .access(hasScope("ROLE_" + ADMIN.name()))
-                                .requestMatchers(PATCH, "/api/v1/admin/**")
-                                    .access(hasScope(ADMIN_UPDATE.getPermission()))
-                                .requestMatchers(DELETE, "/api/v1/admin/**")
-                                    .access(hasScope(ADMIN_DELETE.getPermission()))
-
-                                .anyRequest().authenticated()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/users/register",
+                                "/api/v1/auth/login",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**"
+                        )
+                        .permitAll()
+                        .requestMatchers( "/api/v1/admin/**")
+                            .access(hasScope("ROLE_" + ADMIN.name()))
+                        .requestMatchers(PATCH, "/api/v1/admin/**")
+                            .access(hasScope(ADMIN_UPDATE.getPermission()))
+                        .requestMatchers(DELETE, "/api/v1/admin/**")
+                            .access(hasScope(ADMIN_DELETE.getPermission()))
+                        .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(
-                        oauth2 -> oauth2
-                                .jwt(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())
                 )
-                .sessionManagement(
-                        session -> session
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .cors(cors -> cors
+                        .configurationSource(corsConfiguration())
                 )
                 .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
